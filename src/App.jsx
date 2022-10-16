237a74e1891e4ef270cff5a8a3c59f302a9158a2
Nico Schmock <nicoschmock@web.de>
2022-10-16 13:00:24 +0200

    cleaning up the file

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 
-import HomePage from "./pages/HomePage/HomePage";
-import ProfilePage from "./pages/ProfilePage/ProfilePage";
+import HomePage from "./pages/homepage/homepage";
+import ProfilePage from "./pages/user-profile/user-profile";
 import SignupPage from "./pages/SignupPage/SignupPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 
@@ -49,6 +49,3 @@ function App() {
 }
 
 export default App;
-
-
-// testcdcd
