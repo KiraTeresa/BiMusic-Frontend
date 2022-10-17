a4da8eaf8a9e8fec1024c912e3b9d673a79a994a
Kira <kira.soba@gmx.de>
2022-10-17 20:49:44 +0200

    Feat: disable city/country when isRemote + add country selection from REST api + show create sample form when addSample

---
+++ src/components/CreateSample/CreateSample.jsx
@@ -0,0 +1,7 @@
+function CreateSample() {
+    return <div>
+        <h2>Form to add a new sample</h2>
+    </div>
+}
+
+export default CreateSample
--- src/pages/projects/projects-create.jsx
+++ src/pages/projects/projects-create.jsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import GENRE_ENUM from "../../consts/genreEnum"
 import SKILL_ENUM from "../../consts/skillEnum"
 import { format } from "date-fns"
 import axios from "axios";
+import CreateSample from "../../components/CreateSample/CreateSample"
 
 function ProjectsCreate() {
     const [form, setForm] = useState({
@@ -17,10 +18,24 @@ function ProjectsCreate() {
         city: "",
         country: "",
         initiator: "This will be changed to the ID of the current user!!!", // <-- CHANGES NEED TO BE MADE HERE
+        addSample: false,
         sample: "will be a sample ID", // <-- CHANGES NEED TO BE MADE HERE
     })
     const [genreArr, setGenreArr] = useState([])
     const [skillArr, setSkillArr] = useState([])
+    const [countries, setCountries] = useState([])
+
+    useEffect(() => {
+        axios
+            .get(`https://restcountries.com/v3.1/all`)
+            .then(response => {
+                setCountries(response.data)
+                // console.log('Response from API is: ', response);
+                const countryDetail = response.data[0];
+                console.log('a single country details: ', countryDetail);
+            })
+            .catch(err => console.log(err));
+    }, [])
 
     function handleChange(e) {
         const { name, value } = e.target;
@@ -28,27 +43,33 @@ function ProjectsCreate() {
     }
 
     function handleCheckboxChange(e) {
-        const { name, value, checked } = e.target;
-        let getArr;
-        let setArr;
+        const { name, checked } = e.target;
 
-        if (name === "genre") {
-            getArr = genreArr;
-            setArr = setGenreArr
-        } else if (name === "lookingFor") {
-            getArr = skillArr;
-            setArr = setSkillArr
-        } else if (name === "isRemote") {
-            setForm({ ...form, [name]: !form.isRemote })
-            return;
+        switch (name) {
+            case "genre":
+                setArrayValues(e.target, genreArr, setGenreArr);
+                break;
+            case "lookingFor":
+                setArrayValues(e.target, skillArr, setSkillArr);
+                break;
+            case "isRemote":
+            case "addSample":
+                setForm({ ...form, [name]: checked });
+                break;
+            default:
+                console.log(`No case matching ${name}, sorry.`)
         }
+    }
+
+    function setArrayValues(target, arr, setArr) {
+        const { name, value, checked } = target;
 
         if (checked) {
-            setArr([...getArr, value])
-            setForm({ ...form, [name]: [...getArr, value] })
+            setArr([...arr, value])
+            setForm({ ...form, [name]: [...arr, value] })
         }
         else {
-            const newArr = getArr.filter(e => e !== value)
+            const newArr = arr.filter(e => e !== value)
             setArr(newArr)
             setForm({ ...form, [name]: newArr })
         }
@@ -86,48 +107,69 @@ function ProjectsCreate() {
         e.preventDefault();
         console.log("FORM --> ", form)
 
-        // axios.post("/projects/create", form).then(console.log).catch(console.error)
+        // TO DO -->
+        // create api-client.js in services, set baseURL etc
+        // 
+        // apiClient.post("/projects/create", form).then(console.log).catch(console.error)
     }
 
     return (
         <div>
             <h2>Form to create a new project</h2>
             <form onSubmit={handleSubmit} style={{ display: "flex", flexDirection: "column" }}>
+                {/* Title */}
                 <label>Title
                     <input onChange={handleChange} value={form.title} type="text" name="title"></input>
                 </label>
+                {/* Short Description */}
                 <label>Give a briefe description of your idea:
                     <input onChange={handleChange} value={form.shortDescription} type="text" name="shortDescription" maxLength="150"></input>
                 </label>
+                {/* Long Description */}
                 <label>Now you can explain in more detail:
                     <textarea onChange={handleChange} value={form.longDescription} type="text" name="longDescription" minLength="200" maxLength="1000"></textarea>
                 </label>
+                {/* Genre */}
                 <label>Which genre will your project be?</label>
                 {GENRE_ENUM.map((genre) => {
                     return <label key={genre}><input onChange={handleCheckboxChange} type="checkbox" name="genre" value={genre}></input>{genre}</label>
                 })}
+                {/* Looking For (Skills) */}
                 <label>Who are you looking for?</label>
                 {SKILL_ENUM.map((skill) => {
                     return <label key={skill}><input onChange={handleCheckboxChange} type="checkbox" name="lookingFor" value={skill}></input>{skill}</label>
                 })}
+                {/* Start Date */}
                 <label>When do you wanna start?
                     <input onChange={handleChange} value={form.startDate} type="date" name="startDate"></input>
                 </label>
+                {/* End Date */}
                 <label>Wnen will it be over?
                     <input onChange={handleChange} value={form.endDate} type="date" name="endDate"></input>
                 </label>
+                {/* Remote? */}
                 <label>Will you connect online?
                     <input onChange={handleCheckboxChange} value={form.isRemote} type="checkbox" name="isRemote"></input>
                 </label>
+                {/* City */}
                 <label>Select a city:
-                    <input onChange={handleChange} value={form.city} type="text" name="city" disabled></input> {/* <---- should be disabled when isRemote + API call for dropdown?? */}
+                    <input onChange={handleChange} value={form.city} type="text" name="city" disabled={form.isRemote}></input>
                 </label>
-                <label>Select the country
-                    <input onChange={handleChange} value={form.country} type="text" name="country"></input> {/* <---- should be disabled when isRemote + API call for dropdown?? */}
-                </label>
+                {/* Country */}
+                {/* <label>Select the country */}
+                {/* <input onChange={handleChange} value={form.country} type="text" name="country" disabled={form.isRemote}></input> */}
+                {/* </label> */}
+                <select name="country" onChange={handleChange} disabled={form.isRemote}>
+                    <option value="">-- Select the country --</option>
+                    {countries.map(country => {
+                        return <option key={country.name.common} value={country.name.common}>{country.name.common}</option>
+                    })}
+                </select>
+                {/* Sample */}
                 <label>Do you want to add a sample?
-                    <input onChange={handleChange} value={form.sample} type="text" name="sample"></input> {/* <---- nest another component --> sampleCreate */}
+                    <input onChange={handleCheckboxChange} value={form.addSample} type="checkbox" name="addSample"></input>
                 </label>
+                {form.addSample ? <div style={{ backgroundColor: "grey" }}><CreateSample /></div> : ""}
                 <button type="submit">Create</button>
             </form>
         </div >
