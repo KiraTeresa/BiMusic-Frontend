ba1d423af96778c9172e8d0cca80453c6e08cdf2
Nico Schmock <nicoschmock@web.de>
2022-10-21 15:02:58 +0200

    edit profile done, except skills

--- src/pages/user-profile/EditUserProfile.jsx
+++ src/pages/user-profile/EditUserProfile.jsx
@@ -3,12 +3,10 @@ import { useContext,useEffect, useState } from "react";
 import { AuthContext } from "../../context/auth.context";
 import axios from "axios";
 import SKILL_ENUM from "../../consts/skillEnum"
-import { useNavigate } from 'react-router-dom';
 
 const EditUserProfile = () => {
   const [userInfo,setUserInfo]= useState(undefined)
   const {user} = useContext(AuthContext)//this function will give us the user info
-  
   
   useEffect(() => {
     axios
@@ -18,30 +16,44 @@ const EditUserProfile = () => {
       .then(response => setUserInfo(response.data)).catch((err)=>{console.log(err)});
   }, []);
 
-  const [city, setCity] = useState("");
-  const [country, setCountry] = useState("");
-  const [name, setName] = useState("");
-  const [aboutMe, setAboutMe] = useState("");
+  const [city, setCity] = useState(userInfo && userInfo.city);
+  const [country, setCountry] = useState(userInfo && userInfo.country);
+  const [name, setName] = useState(userInfo && userInfo.name);
+  const [aboutMe, setAboutMe] = useState(userInfo && userInfo.aboutMe);
   const [skillArr, setSkillArr] = useState([])
+  const [filterSkillArr, setFilterSkillArr] = useState([])
   const [errorMessage, setErrorMessage] = useState(undefined);
-
-  const navigate = useNavigate();
-
-
   const handleName = (e) => setName(e.target.value);
   const handleCity = (e) => setCity(e.target.value);
   const handleAboutMe = (e) => setAboutMe(e.target.value);
   const handleCountry = (e) => setCountry(e.target.value);
+  
+  useEffect(() => {
+  let skillEnum2 = [...SKILL_ENUM];
+  let filterSkillEnum = skillEnum2.filter(item=>{
+    return (userInfo && !userInfo.skills.includes(item))
+  })
+  setFilterSkillArr(filterSkillEnum);
+  }, [userInfo])
 
-  console.log(userInfo)
-
-let skillEnum2 = [...SKILL_ENUM]
-let filterSkillEnum = skillEnum2.filter(item=>!userInfo.skills.includes(item))
-
+const handleUserProfileUpdate =async(e)=> {
+  try{
+    e.preventDefault();
+    let updatedData={name,city,country,aboutMe,email:userInfo.email};
+    //Updating data to database
+    const response= await axios.put("http://localhost:5005/profile/editinfo",updatedData);
+    //Fetching data again
+    const updatedInfo=await axios.post("http://localhost:5005/profile/",{email:user.email});
+    setUserInfo(updatedInfo.data);
+  }catch(err){
+    console.log(err);
+  }
+}
   return (
 <div>
       {userInfo ?
-        <form>
+      <>
+        <form onSubmit={handleUserProfileUpdate}>
           <label>Name:</label>
           <input onChange={handleName} type="text" name="name" defaultValue={userInfo.name}/> 
 
@@ -54,16 +66,24 @@ let filterSkillEnum = skillEnum2.filter(item=>!userInfo.skills.includes(item))
 
         <label>About me:</label>
         <input onChange={handleAboutMe} type="text" name="country" defaultValue={userInfo.aboutMe} />
+        <button type="submit">Update</button>
+        </form>
 
-        {/* <label>What kind of musician are you?</label>
-        {filterSkillEnum.map((skill) => {
+        <label>What kind of musician are you?</label>
+        <div> <h2>Render skill already selected: {userInfo.skills.map((skill)=>(
+        <div>
+        <p>{skill}</p>
+        <button>Delete</button>
+        </div>
+        ))}
+        </h2>
+        </div>
+        {filterSkillArr && filterSkillArr.map((skill) => {
           return <label key={skill}>
             <input type="checkbox" name="lookingFor" value={skill}>
             </input>{skill}</label>
-        })} */}
-
-        <button type="submit">Sign Up</button>
-      </form>
+        })}
+      </>
 :<div>noData</div>}
 </div>
   )
