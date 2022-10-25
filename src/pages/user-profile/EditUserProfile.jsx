ab7bd9936f0860b37659e4176fb8f5d6dbe47492
Nico Schmock <nicoschmock@web.de>
2022-10-25 19:36:37 +0200

    account settings

--- src/pages/user-profile/EditUserProfile.jsx
+++ src/pages/user-profile/EditUserProfile.jsx
@@ -1,6 +1,6 @@
 import "./user-profile-edit.css";
 import React from 'react'
-import { useContext,useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../context/auth.context";
 import axios from "axios";
 import SKILL_ENUM from "../../consts/skillEnum"
@@ -8,17 +8,12 @@ import { Link } from "react-router-dom";
 import apiClient from "../../services/apiClient";
 
 const EditUserProfile = () => {
-  const [userInfo,setUserInfo]= useState(undefined);
-  const [userProject,setUserProject]= useState(undefined);
-  const {user} = useContext(AuthContext)//this function will give us the user info
-  
-  useEffect(() => {
-    axios
-      .post("http://localhost:5005/profile/",{
-        email:user.email
-      } )
-      .then(response => setUserInfo(response.data)).catch((err)=>{console.log(err)});
-  }, []);
+  const [userInfo, setUserInfo] = useState(undefined);
+  const [userProject, setUserProject] = useState(undefined);
+  const [userAvatar, setUserAvatar] = useState(undefined);
+  const [countries, setCountries] = useState(undefined);
+  const [cities, setCities] = useState(undefined);
+  const { user } = useContext(AuthContext)//this function will give us the user info
 
   const [city, setCity] = useState(userInfo && userInfo.city);
   const [country, setCountry] = useState(userInfo && userInfo.country);
@@ -27,174 +22,248 @@ const EditUserProfile = () => {
   const [skillArr, setSkillArr] = useState([])
   const [filterSkillArr, setFilterSkillArr] = useState([])
   const [errorMessage, setErrorMessage] = useState(undefined);
+
   const handleName = (e) => setName(e.target.value);
-  const handleCity = (e) => setCity(e.target.value);
+
+  const handleCity = (e) => {
+    console.log(e.target.value);
+    setCity(e.target.value)
+  };
+
   const handleAboutMe = (e) => setAboutMe(e.target.value);
-  const handleCountry = (e) => setCountry(e.target.value);
-  
+
+  const handleCountry = (e) => {
+    setCountry(e.target.value);
+    const findCountry = countries.find((el) => {
+      return el.country === e.target.value
+    });
+    if (findCountry) {
+      console.log(findCountry.cities);
+      setCities(findCountry.cities);
+    }
+  };
+  console.log(cities);
+
   useEffect(() => {
-  let skillEnum2 = [...SKILL_ENUM];
-  let filterSkillEnum = skillEnum2.filter(item=>{
-    return (userInfo && !userInfo.skills.includes(item))
-  })
-  setFilterSkillArr(filterSkillEnum);
+    let skillEnum2 = [...SKILL_ENUM];
+    let filterSkillEnum = skillEnum2.filter(item => {
+      return (userInfo && !userInfo.skills.includes(item))
+    })
+    setFilterSkillArr(filterSkillEnum);
   }, [userInfo]);
 
   useEffect(() => {
-    axios
-      .get(`http://localhost:5005/profile/addedproject/${user._id}`)
+    apiClient
+      .get(`/profile/addedproject/${user._id}`)
       .then(response => {
         setUserProject(response.data)
       })
-      .catch((err)=>{
+      .catch((err) => {
         console.log(err)
       });
-  }, [userProject]);
+  }, []);
 
-const handleUserProfileUpdate =async(e)=> {
-  try{
-    e.preventDefault();
-    let updatedData={name,city,country,aboutMe,email:userInfo.email};
-    //Updating data to database
-    const response= await axios.put("http://localhost:5005/profile/editinfo",updatedData);
-    //Fetching data again
-    const updatedInfo=await axios.post("http://localhost:5005/profile/",{email:user.email});
-    setUserInfo(updatedInfo.data);
-  }catch(err){
-    console.log(err);
-  }
-}
-
-
-
-const handleAddSkills = async(e,skill)=>{
-  try{
-  e.preventDefault();
-  console.log(skill);
-  let updatedData={skill,email:userInfo.email};
-    //Updating data to database
-    const response= await axios.put("http://localhost:5005/profile/editskill",updatedData);
-    //Fetching data again
-    const updatedInfo=await axios.post("http://localhost:5005/profile/",{email:user.email});
-    setUserInfo(updatedInfo.data);
-  }catch(err){
-    console.log(err);
+  const handleUserProfileUpdate = async (e) => {
+    try {
+      e.preventDefault();
+      let updatedData = { name, city, country, aboutMe, email: userInfo.email };
+      //Updating data to database
+      const response = await apiClient.put("/profile/editinfo", updatedData);
+      //Fetching data again
+      const updatedInfo = await apiClient.post("/profile/", { email: user.email });
+      setUserInfo(updatedInfo.data);
+    } catch (err) {
+      console.log(err);
+    }
   }
 
-}
 
-const handleDeleteSkills = async(e,skill)=>{
-  try{
-  e.preventDefault();
-  let updatedData={skill,email:userInfo.email};
-    //Updating data to database
-    const response= await axios.put("http://localhost:5005/profile/deleteskill",updatedData);
-    //Fetching data again
-    const updatedInfo=await axios.post("http://localhost:5005/profile/",{email:user.email});
-    setUserInfo(updatedInfo.data);
-  }catch(err){
-    console.log(err);
+
+  const handleAddSkills = async (e, skill) => {
+    try {
+      e.preventDefault();
+      console.log(skill);
+      let updatedData = { skill, email: userInfo.email };
+      //Updating data to database
+      const response = await apiClient.put("/profile/editskill", updatedData);
+      //Fetching data again
+      const updatedInfo = await apiClient.post("/profile/", { email: user.email });
+      setUserInfo(updatedInfo.data);
+    } catch (err) {
+      console.log(err);
+    }
+
   }
-}
 
-const handleDeleteProject=async(e,id)=>{
-  try{
-    e.preventDefault();
-    let projectId=id;
-    //Updating data to database
-    await apiClient.post(`/projects/${projectId}/delete`);
-    //Fetching data again
-    const updatedProject=await axios.get(`http://localhost:5005/profile/addedproject/${user._id}`);
-    setUserProject(updatedProject.data);
-  }catch(error){
-    console.log(error)
+  const handleDeleteSkills = async (e, skill) => {
+    try {
+      e.preventDefault();
+      let updatedData = { skill, email: userInfo.email };
+      //Updating data to database
+      const response = await apiClient.put("/profile/deleteskill", updatedData);
+      //Fetching data again
+      const updatedInfo = await apiClient.post("/profile/", { email: user.email });
+      setUserInfo(updatedInfo.data);
+    } catch (err) {
+      console.log(err);
+    }
   }
-}
 
+  const handleDeleteProject = async (e, id) => {
+    try {
+      e.preventDefault();
+      let projectId = id;
+      //Updating data to database
+      await apiClient.post(`/projects/${projectId}/delete`);
+      //Fetching data again
+      const updatedProject = await apiClient.get(`/profile/addedproject/${user._id}`);
+      setUserProject(updatedProject.data);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const handleAvatarUpdate = async (e) => {
+    try {
+      e.preventDefault();
+      const formData = new FormData();
+      formData.append("file", userAvatar);
+      formData.append("upload_preset", "BiMusic")
+      const response = await axios.post(`https://api.cloudinary.com/v1_1/da02iubhb/image/upload`, formData);
+      const avatarData = { avatar: response.data.url, cloudinary_id: response.data.public_id, email: userInfo.email }
+      const uploadedAvatar = await apiClient.put("/profile/uploadavatar", avatarData)
+      //Fetching data again
+      const updatedInfo = await apiClient.post("/profile/", { email: user.email });
+      setUserInfo(updatedInfo.data);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    axios
+      .get(`https://countriesnow.space/api/v0.1/countries`)
+      .then(response => {
+        const resArray = response.data.data;
+        setCountries(resArray);
+      }
+      ).catch((err) => { console.log(err); })
+  }, [])
+
+  useEffect(() => {
+    apiClient
+      .post("/profile/", {
+        email: user.email
+      })
+      .then(response => setUserInfo(response.data)).catch((err) => { console.log(err) });
+  }, []);
 
   return (
-<div class="container">
+    <div className="container">
       {userInfo ?
-      <>
-      {/* Name Field */}
-        <form onSubmit={handleUserProfileUpdate}>
-        <div className="row">
-        <div className="col-25">
-          <label>Name:</label>
-        </div>
-        <div className="col-75">
-          <input onChange={handleName} type="text" name="name" defaultValue={userInfo.name}/> 
-          </div>
-          </div>
+        <>
+          {/* Name Field */}
+          <form onSubmit={handleUserProfileUpdate}>
+            <div className="row">
+              <div className="col-25">
+                <label>Name:</label>
+              </div>
+              <div className="col-75">
+                <input onChange={handleName} type="text" name="name" defaultValue={userInfo.name} />
+              </div>
+            </div>
 
-        {/* City */}
-        <div className="row">
-        <div className="col-25">
-        <label>City:</label>
-        </div>
-        <div className="col-75">
-        <input onChange={handleCity} type="text" name="city" defaultValue={userInfo.city} />
-        </div>
-        </div>
-    
-        {/* Country */}
-        <div className="row">
-        <div className="col-25">
-        <label>Country:</label>
-        </div>
-        <div className="col-75">
-        <input onChange={handleCountry} type="text" name="country" defaultValue={userInfo.country} />
-        </div>
-        </div>
-        
-        {/* About me */}
-        <div className="row">
-        <div className="col-25">
-        <label>About me:</label>
-        </div>
-        <div className="col-75">
-        <textarea style={{height:"150px"}} onChange={handleAboutMe} type="text" name="country" defaultValue={userInfo.aboutMe} />
-        </div>
-        </div>
+            {/* Country */}
+            <div className="row">
+              <div className="col-25">
+                <label>Country:</label>
+              </div>
+              <div className="col-75">
+                {/* <input onChange={handleCountry} type="text" name="country" defaultValue={userInfo.country} /> */}
+              </div>
+            </div>
 
-      {/* Update (Name,City,Country,AboutMe) Button */}
-        <button type="submit">Update</button>
-        </form>
+            <label>-- Select the country --</label>
+            <select name="country" onChange={handleCountry}>
+              <option value={userInfo.country}> -- {userInfo.country} -- </option>
+              {countries ? countries.map((element, index) => {
+                return <option key={index} value={element.country}>{element.country}</option>
+              }) : <option >Select a country</option>}
+            </select>
+
+            {/* City */}
+            <div className="row">
+              <div className="col-25">
+                <label>City:</label>
+              </div>
+              <div className="col-75">
+                {/* <input onChange={handleCity} type="text" name="city" defaultValue={userInfo.city} /> */}
+              </div>
+            </div>
+
+            <label>-- Select the city --</label>
+            <select name="city" onChange={handleCity}>
+              <option value={userInfo.city}> -- {userInfo.city} -- </option>
+              {cities && cities.map((element, index) => {
+                return <option key={index} value={element}>{element}</option>
+              })}
+            </select>
+
+            {/* About me */}
+            <div className="row">
+              <div className="col-25">
+                <label>About me:</label>
+              </div>
+              <div className="col-75">
+                <textarea style={{ height: "150px" }} onChange={handleAboutMe} type="text" name="country" defaultValue={userInfo.aboutMe} />
+              </div>
+            </div>
+
+            {/* Update (Name,City,Country,AboutMe) Button */}
+            <button type="submit">Update</button>
+          </form>
 
           {/* Update Current Skillset */}
-         <h2>Update your current skillset:</h2>
-        <div className="borderFrame">
-        {userInfo && userInfo.skills.map((skill,index)=>(
-        <div key={index}>
-        <span className="currentSkill">{skill}</span>
-        <button onClick={(e)=>{handleDeleteSkills(e,skill)}}>Delete</button>
-        </div>
-        ))}
-</div>
-<h2>Add new skills to your profile:</h2>
-        {filterSkillArr && filterSkillArr.map((skill) => {
-          return <label key={skill}>
-            <input onChange={(e)=>{handleAddSkills(e,skill)}} type="checkbox" name="lookingFor" value={skill}>
-            </input>{skill}</label>
-        })}
-    {/* Update created Projects */}
-        <div>
-          <h2>Created projects by you: </h2>
+          <h2>Update your current skillset:</h2>
           <div className="borderFrame">
-          {userProject && userProject.map((project,index)=>(
-            <div>
-         <Link to={`/projects/${project._id}`} key={index}>
-          {project.title}
-          </Link>
-          <button  onClick={(e)=>{handleDeleteProject(e,project._id)}}>Delete</button>
+            {userInfo && userInfo.skills.map((skill, index) => (
+              <div key={index}>
+                <span className="currentSkill">{skill}</span>
+                <button onClick={(e) => { handleDeleteSkills(e, skill) }}>Delete</button>
+              </div>
+            ))}
           </div>
-          ))}
+          <h2>Add new skills to your profile:</h2>
+          {filterSkillArr && filterSkillArr.map((skill) => {
+            return <label key={skill}>
+              <input onChange={(e) => { handleAddSkills(e, skill) }} type="checkbox" name="lookingFor" value={skill}>
+              </input>{skill}</label>
+          })}
+          {/* Update created Projects */}
+          <div>
+            <h2>Created projects by you: </h2>
+            <div className="borderFrame">
+              {userProject && userProject.map((project, index) => (
+                <div>
+                  <Link to={`/projects/${project._id}`} key={index}>
+                    {project.title}
+                  </Link>
+                  <button onClick={(e) => { handleDeleteProject(e, project._id) }}>Delete</button>
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
-       
-      </>
-:<div>noData</div>}
-</div>
+          <div>
+            <img src={userInfo && userInfo.avatar} alt="avatar" width={"300px"} />
+          </div>
+          <form onSubmit={handleAvatarUpdate}>
+            <input type="file" onChange={(e) => setUserAvatar(e.target.files[0])} />
+            <input type="submit" />
+          </form>
+
+        </>
+        : <div>noData</div>}
+    </div>
   )
 }
 
--- src/pages/user-profile/user-profile.jsx
+++ src/pages/user-profile/user-profile.jsx
@@ -1,24 +1,23 @@
 import "./user-profile.css";
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../context/auth.context";
-import axios from "axios";
+import apiClient from '../../services/apiClient';
 import { Link } from "react-router-dom";
 
 function ProfilePage() {
-  const [userInfo,setUserInfo]= useState(null);
-  const [userProject,setUserProject]= useState(null);
+  const [userInfo, setUserInfo] = useState(null);
+  const [userProject, setUserProject] = useState(null);
 
-  const {user} = useContext(AuthContext)//this function will give us the user info
-  
+  const { user } = useContext(AuthContext)//this function will give us the user info
+
   useEffect(() => {
-    axios
-      .post("http://localhost:5005/profile/",{
-        email:user.email
-      } )
+    apiClient.post("/profile/", {
+        email: user.email
+      })
       .then(response => {
         setUserInfo(response.data)
       })
-      .catch((err)=>{
+      .catch((err) => {
         console.log(err)
       });
   }, []);
@@ -26,12 +25,12 @@ function ProfilePage() {
   console.log(userProject);
 
   useEffect(() => {
-    axios
-      .get(`http://localhost:5005/profile/addedproject/${user._id}`)
+    apiClient
+      .get(`/profile/addedproject/${user._id}`)
       .then(response => {
         setUserProject(response.data)
       })
-      .catch((err)=>{
+      .catch((err) => {
         console.log(err)
       });
   }, []);
@@ -40,30 +39,31 @@ function ProfilePage() {
     <div>
       <h2>Your profile</h2>
       {userInfo &&
-<div>
-        <h4>Forum Username:{userInfo.name}</h4>
-      <h4>Email:{userInfo.email}</h4>
-      <h5>{userInfo.city} </h5>  
-      <h5>{userInfo.country} </h5>
-      <h4>About me:{userInfo.aboutMe} </h4>
-      <h4>Skills:</h4>
-  {userInfo.skills}
-        <h4>
-          My Projects:
-        </h4>
-        {userProject && userProject.map((project,index)=>(
-          <Link to={`/projects/${project._id}`} key={index}>
-           <p>
-             {project.title}
-            </p>
-        </Link>
-        ))}
-      <h4>My Samples: </h4>
-      {/* <h1>collabProjects:{user? user.collabProjects:""} </h1>
+        <div>
+          <h4>Forum Username:{userInfo.name}</h4>
+          <h4>Email:{userInfo.email}</h4>
+          <h5>{userInfo.city} </h5>
+          <h5>{userInfo.country} </h5>
+          <h4>About me:{userInfo.aboutMe} </h4>
+          <h4>Skills:</h4>
+          {userInfo.skills}
+          <h4>
+            My Projects:
+          </h4>
+          {userProject && userProject.map((project, index) => (
+            <Link to={`/projects/${project._id}`} key={index}>
+              <p>
+                {project.title}
+              </p>
+            </Link>
+          ))}
+          <h4>My Samples: </h4>
+          {/* <h1>collabProjects:{user? user.collabProjects:""} </h1>
     <h1>samples:</h1> */}
-  </div>
-  }
+        </div>
+      }
       <Link to='/editprofile'><button>Edit Profile</button></Link>
+      <Link to='/account-settings'><button>Account Settings</button></Link>
     </div>
   );
 }
