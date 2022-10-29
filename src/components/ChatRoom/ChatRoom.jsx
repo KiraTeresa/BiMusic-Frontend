689dbdda2f40aee8f2f3c138f93b03d14e85a213
Kira <kira.soba@gmx.de>
2022-10-29 19:38:40 +0200

    Feat: creating project chatrooms // msg history gets stored

---
+++ src/components/ChatRoom/ChatRoom.jsx
@@ -0,0 +1,80 @@
+import { useEffect, useState, useMemo } from "react";
+import { useAuth } from "../../context/auth.context"
+import apiClient from "../../services/apiClient";
+import Loading from '../../components/Loading/Loading';
+
+function ChatRoom({ chatId }) {
+    const { user } = useAuth() // <-- returns logged-in user (_id, email, name)
+    const [projectInfo, setProjectInfo] = useState({})
+    const [dbHistory, setDbHistory] = useState([])
+    const [message, setMessage] = useState({ msg: "", user: user.name, userId: user._id, chat: chatId })
+    const [msgHistory, setMsgHistory] = useState([])
+    const [isLoading, setIsLoading] = useState(true);
+    const ws = useMemo(() => new WebSocket("ws://localhost:8082"), [])
+    // const newMessage = {author: "currentUserId", msg: "", time: new Date()}
+
+    console.log("Chat Id: ", chatId)
+
+    useEffect(() => {
+        console.log("Frontend welcomes you in the chat.")
+
+        // connect to wsServer:
+        ws.addEventListener("open", () => {
+            console.log("We are connected")
+        })
+    }, [ws])
+
+    useEffect(() => {
+        // go to the chatroom:
+        apiClient.get(`/chats/${chatId}`).then((result) => {
+            console.log("Chat room: you are logged in ", result)
+            setProjectInfo(result.data.project)
+            setDbHistory(result.data.history)
+        }).catch(() => console.log("Could not log you in at the chat")).finally(() => setIsLoading(false))
+    }, [])
+
+    function handleChange(e) {
+        setMessage({ ...message, msg: e.target.value })
+    }
+
+    async function sendMessage(e) {
+        e.preventDefault()
+        ws.send(JSON.stringify(message))
+        console.log("Message sent ", message.msg)
+
+        await apiClient.post("/message", message).then(() => console.log("Added your message to collection.")).catch(() => console.log("Couldn't add your msg to collection --- "))
+
+        setMessage({ ...message, msg: "" })
+    }
+
+
+    ws.addEventListener("message", e => {
+        const dataFromServer = JSON.parse(e.data)
+        console.log("We received a message: ", dataFromServer)
+        setMsgHistory([...msgHistory, dataFromServer])
+    })
+
+    if (isLoading) {
+        return <Loading />
+    }
+
+    return (
+        <div>
+            <h2>Chatroom: {projectInfo.title}</h2>
+            {dbHistory.length > 0 ? dbHistory.map((element) => {
+                return <p key={element._id} style={{ backgroundColor: element.author.name === user.name ? "#63A18F" : "grey" }}><strong>{element.author.name}:</strong> {element.text}</p>
+            }) : ""}
+            {msgHistory.length > 0 ?
+                msgHistory.map((element, index) => {
+                    return <p key={index} style={{ backgroundColor: element.user === user.name ? "#63A18F" : "grey" }}><strong>{element.user}:</strong> {element.msg}</p>
+                })
+                : <p>... no new messages ...</p>}
+            <form onSubmit={sendMessage}>
+                <input type="text" name="msg" onChange={handleChange} value={message.msg}></input>
+                <button>send msg</button>
+            </form>
+        </div>
+    )
+}
+
+export default ChatRoom;
--- src/pages/chat/chat.jsx
+++ src/pages/chat/chat.jsx
@@ -2,69 +2,91 @@ import { useEffect, useState, useMemo } from "react";
 import { useAuth } from "../../context/auth.context"
 import apiClient from "../../services/apiClient";
 import Loading from '../../components/Loading/Loading';
+import ChatRoom from "../../components/ChatRoom/ChatRoom";
 
 function Chat() {
     const { user } = useAuth() // <-- returns logged-in user (_id, email, name)
     const [message, setMessage] = useState('')
     const [msgHistory, setMsgHistory] = useState([])
+    const [activeChat, setActiveChat] = useState(undefined)
     const [isLoading, setIsLoading] = useState(true);
-    const ws = useMemo(() => new WebSocket("ws://localhost:8082"), [])
+    const [usersProjects, setUsersProjects] = useState([])
+    const [usersChats, setUsersChats] = useState([])
+    const [newChat, setNewChat] = useState({})
+    // const ws = useMemo(() => new WebSocket("ws://localhost:8082"), [])
     // const newMessage = {author: "currentUserId", msg: "", time: new Date()}
 
-    useEffect(() => {
-        console.log("Frontend welcomes you in the chat.")
+    // useEffect(() => {
+    //     console.log("Frontend welcomes you in the chat.")
 
-        // connect to wsServer:
-        ws.addEventListener("open", () => {
-            console.log("We are connected")
-        })
-    }, [ws])
+    //     // connect to wsServer:
+    //     ws.addEventListener("open", () => {
+    //         console.log("We are connected")
+    //     })
+    // }, [ws])
 
     useEffect(() => {
         // "login" to the chat:
-        apiClient.get("/chats").then(() => console.log("You are logged in to the chat room")).catch(() => console.log("Could not log you in at the chat")).finally(() => setIsLoading(false))
+        apiClient.get("/chats").then((result) => {
+            // console.log("You are logged in to the chat room")
+            const { allProjects, existingChats } = result.data
+            console.log("Got this result from server: ", result.data)
+            setUsersProjects(allProjects)
+            setUsersChats(existingChats)
+        }).catch(() => console.log("Could not log you in at the chat")).finally(() => setIsLoading(false))
     }, [])
 
     function handleChange(e) {
-        // console.log("---> ", e.target.value)
-        setMessage(e.target.value)
+        const { value } = e.target
+        setNewChat(value)
     }
 
-    function handleSubmit(e) {
+    function createNewChat(e) {
         e.preventDefault()
-        ws.send(JSON.stringify({ msg: message, user: user.name }))
-        console.log("Message sent ", message)
-        setMessage("")
+
+        apiClient.post("/chats", { newChat }).then((result) => console.log("Chat created ", result)).catch(() => console.log("uups"))
     }
 
-
-    ws.addEventListener("message", e => {
-        const dataFromServer = JSON.parse(e.data)
-        console.log("We received a message: ", dataFromServer)
-        setMsgHistory([...msgHistory, dataFromServer])
-    })
+    function openChat(e) {
+        setActiveChat(e.target.value)
+    }
 
     if (isLoading) {
         return <Loading />
     }
 
+    // console.log("Here ", usersProjects)
+    console.log("Selected proj: ", newChat)
+
     return (
-        <section>
-            <h2>Chat</h2>
-            <div>
-                {msgHistory.length > 0 ?
-                    msgHistory.map((element, index) => {
-                        return <p key={index} style={{ backgroundColor: element.user === user.name ? "#63A18F" : "grey" }}><strong>{element.user}:</strong> {element.msg}</p>
-                    })
-                    : <p>... no messages ...</p>}
-                <form onSubmit={handleSubmit}>
-                    <input type="text" name="msg" onChange={handleChange} value={message}></input>
-                    <button>send msg</button>
+        <section style={{ display: "flex", justifyContent: "space-between", maxWidth: "1290px", margin: "auto" }}>
+            <div style={{ display: "flex", flexDirection: "column", width: "200px" }}>
+                <h5>Your chatrooms</h5>
+                {usersChats.map((chat) => {
+                    return <button key={chat._id} value={chat._id} onClick={openChat}>{chat.project.title}</button>
+                })}
+                <hr></hr>
+                <p>Create a chat for another project:</p>
+                <form onSubmit={createNewChat}>
+                    <select name="project" onChange={handleChange}>
+                        {usersProjects.map((proj) => {
+                            return <option key={proj._id} value={proj._id}>{proj.title}</option>
+                        })}
+                    </select>
+                    <button type="submit" style={{ backgroundColor: "#63A18F" }}>create new room</button>
                 </form>
             </div>
-            <article>
-                <h3>WebSocket Data</h3>
-            </article>
+            {activeChat ?
+                <div>
+                    <h2>Chat</h2>
+                    <ChatRoom chatId={activeChat} />
+                    <div>
+                    </div>
+                </div>
+                : ""}
+            <div style={{ display: "flex", width: "200px" }}>
+                <h5>Chat members</h5>
+            </div>
         </section>
     )
 }
