f6e5a9c8c217baa294b714deb3316e94d6d80326
Kira <kira.soba@gmx.de>
2022-11-03 10:49:56 +0100

    Update: show users skills in chat member list

--- src/components/Chat/ChatMemberCard.jsx
+++ src/components/Chat/ChatMemberCard.jsx
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router-dom";
 
 function ChatMemberCard({ userInfo }) {
-    const { _id, name, avatar } = userInfo
+    const { _id, name, avatar, skills } = userInfo
     const navigate = useNavigate()
 
     const goToUserProfile = () => {
@@ -9,12 +9,18 @@ function ChatMemberCard({ userInfo }) {
     }
 
     return (
-        <div onClick={goToUserProfile} className="chat-member">
-            <div className="avatar-wrapper">
-                <img src={avatar ? avatar : "https://upload.wikimedia.org/wikipedia/commons/3/3f/Placeholder_view_vector.svg"} alt="user avatar" />
+        <div onClick={goToUserProfile} className="test-member-wrapper">
+            <div className="chat-member">
+                <div className="avatar-wrapper">
+                    <img src={avatar ? avatar : "https://upload.wikimedia.org/wikipedia/commons/3/3f/Placeholder_view_vector.svg"} alt="user avatar" />
+                </div>
+                <p>{name}</p>
             </div>
-
-            <p>{name}</p>
+            <div className="skills">
+                {skills.map((skill) => {
+                    return <p className="skill" key={skill}>{skill}</p>
+                })}
+            </div>
         </div>
     )
 }
