2ca0f1ed008700095230f48a425e73f786c6422c
Kira <kira.soba@gmx.de>
2022-11-10 12:13:32 +0100

    Style: project cards [DONE]

--- src/components/Project/ProjectCard.jsx
+++ src/components/Project/ProjectCard.jsx
@@ -11,49 +11,63 @@ function ProjectCard({ project, backgroundColor }) {
         <div className="project-card" style={{ backgroundColor }}>
             <Link to={`/projects/${_id}`}>
                 <div className='card-head'>
-                    <p className='title'>{title}</p>
 
                     <div className='main-info'>
                         <div className='about'>
+                            <p className='title'>{title}</p>
                             <p className='shortD'> {shortDescription}</p>
                         </div>
-                        <div className='looking-for'>
+                        <div className='icon-wrapper'>
+                            <div>
+                                <img className="icon" src={commentIcon} alt="comment icon" />{comments ? comments.length : "0"}
+                            </div>
+                            <div>
+                                <img className="icon" src={contributorsIcon} alt="contributors icon" />{collaborators ? collaborators.length : "0"}
+                            </div>
+                            {sample ?
+                                <div>
+                                    <img className={`icon`} src={sampleIcon} alt="sample icon" />
+                                </div>
+                                : ""}
+                        </div>
+                        {/* <div className='looking-for'>
                             <div>{initiator.name} is looking for </div>
                             <div className="item-wrapper">
                                 {lookingFor.map((skill) => {
                                     return <p key={skill} className="skill"> {skill} </p>
                                 })}
                             </div>
-                        </div>
+                        </div> */}
                     </div>
                 </div>
                 {/* <div className="date-location">
                     <p className='date'>{startDate.slice(0, -14)} - {endDate.slice(0, -14)}</p>
                     <p className='location'>{isRemote ? "online" : (city + ", " + country)}</p>
                 </div> */}
-                <div className='card-body'>
-                    {/* <div className='looking-for'> */}
-                    {/* <img src={initiator.avatar} alt="user avatar" /> */}
-                    {/* </div> */}
-                    <div className='item-wrapper'>
+                {/* <div className='card-body'> */}
+                {/* <div className='looking-for'> */}
+                {/* <img src={initiator.avatar} alt="user avatar" /> */}
+                {/* </div> */}
+                {/* <div className='item-wrapper'>
                         {/* <h4>Genre:</h4> */}
-                        {genre.map((g) => {
-                            return <p key={g}>{g}</p>
-                        })}
-                        {/* <h4>{initiator.name} is looking for a </h4>
+                {/* {genre.map((g) => {
+                        return <p key={g}>{g}</p>
+                    })}  */}
+                {/* <h4>{initiator.name} is looking for a </h4>
                         {lookingFor.map((skill) => {
                             return <p key={skill} className="skill"> {skill} </p>
                         })} */}
-                    </div>
-                </div>
+                {/* </div> */}
+                {/* </div> */}
                 <div className="card-bottom">
-                    <p>Posted by <span className="initiator">{initiator.name}</span></p>
-                    <div>
-                        <img className="icon" src={commentIcon} alt="comment icon" />{comments ? comments.length : "0"}
-                        <img className="icon" src={contributorsIcon} alt="contributors icon" />{collaborators ? collaborators.length : "0"}
-                        {sample ?
-                            <img className={`icon`} src={sampleIcon} alt="sample icon" /> : ""}
-                    </div>
+                    {/* <div className="item-wrapper"> */}
+                    <span className="initiator">{initiator.name}</span><span>is looking for a</span>
+                    {/* <div className='skill-wrapper'> */}
+                    {lookingFor.map((skill) => {
+                        return <span key={skill} className="skill">{skill}</span>
+                    })}
+                    {/* </div> */}
+                    {/* </div> */}
                 </div>
                 {/* <button className='btn primary'>Read more about this project</button> */}
             </Link>
