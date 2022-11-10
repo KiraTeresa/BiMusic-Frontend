fde84f5692630f3b5432a2ff6f9d4de4378106ae
Kira <kira.soba@gmx.de>
2022-11-10 12:27:14 +0100

    Style: responsive project filter [DONE]

--- src/components/Project/ProjectFilter.jsx
+++ src/components/Project/ProjectFilter.jsx
@@ -158,38 +158,45 @@ function ProjectFilter({ allProjects, sendToParent }) {
                 <input type="text" name="text" value={search.text} onChange={handleFilterChange} placeholder=" -- type here to filter by text --"></input>
             </label>
 
-            {/* Genre */}
-            <select value={search.genre} name="genre" onChange={handleFilterChange}>
-                <option value=""> -- filter by genre --</option>
-                {genreFilter.map(genre => {
-                    return <option key={genre} value={genre}>{genre}</option>
-                })}
-            </select>
+            <div className='wrap-4'>
+                <div className="wrap-2">
+                    {/* Genre */}
+                    <select value={search.genre} name="genre" onChange={handleFilterChange}>
+                        <option value=""> -- filter by genre --</option>
+                        {genreFilter.map(genre => {
+                            return <option key={genre} value={genre}>{genre}</option>
+                        })}
+                    </select>
 
-            {/* Skill */}
-            <select value={search.lookingFor} name="lookingFor" onChange={handleFilterChange}>
-                <option value=""> -- filter by skill --</option>
-                {skillFilter.map(skill => {
-                    return <option key={skill} value={skill}>{skill}</option>
-                })}
-            </select>
+                    {/* Skill */}
+                    <select value={search.lookingFor} name="lookingFor" onChange={handleFilterChange}>
+                        <option value=""> -- filter by skill --</option>
+                        {skillFilter.map(skill => {
+                            return <option key={skill} value={skill}>{skill}</option>
+                        })}
+                    </select>
+                </div>
 
-            {/* Country */}
-            <select value={search.country} name="country" onChange={handleFilterChange}>
-                <option value=""> -- filter by country --</option>
-                {hasRemoteOption ? <option value="isRemote">{'>>'} online</option> : ""}
-                {countryFilter.map(country => {
-                    return <option key={country} value={country}>{country}</option>
-                })}
-            </select>
+                <div className="wrap-2">
+                    {/* Country */}
+                    <select value={search.country} name="country" onChange={handleFilterChange}>
+                        <option value=""> -- filter by country --</option>
+                        {hasRemoteOption ? <option value="isRemote">{'>>'} online</option> : ""}
+                        {countryFilter.map(country => {
+                            return <option key={country} value={country}>{country}</option>
+                        })}
+                    </select>
 
-            {/* City */}
-            <select value={search.city} name="city" onChange={handleFilterChange} disabled={search.country === "isRemote"}>
-                <option value=""> -- filter by city --</option>
-                {cityFilter.map(city => {
-                    return <option key={city} value={city}>{city}</option>
-                })}
-            </select>
+                    {/* City */}
+                    <select value={search.city} name="city" onChange={handleFilterChange} disabled={search.country === "isRemote"}>
+                        <option value=""> -- filter by city --</option>
+                        {cityFilter.map(city => {
+                            return <option key={city} value={city}>{city}</option>
+                        })}
+                    </select>
+                </div>
+            </div>
+
 
             {/* Reset */}
             <button className="btn tertiary reset" onClick={resetFilter}>reset filter</button>
